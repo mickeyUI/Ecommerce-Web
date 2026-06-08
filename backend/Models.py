3ac12b23d76b6ebdf98bb16b68e4b7cfaf81efb2
mickeyUI <140637453+mickeyUI@users.noreply.github.com>
2026-06-08 22:42:39 +0300

    setup backend folder structure and sqlite database

---
+++ backend/Models.py
@@ -0,0 +1,25 @@
+from DB import Base
+from sqlalchemy import Column, Integer, String, Float, Text, Path, ForeignKey
+
+
+
+class Products(Base):
+    __tablename__ = "Products"
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable= False)
+    description = Column(Text, nullable= True)
+    category = Column(String, nullable= False)
+    brand = Column(String, nullable= False)
+    image = Column(Path, nullable= False)
+    price = Column(Float, nullable= False)
+
+class Inventory(Base):
+    __tablename__ = "Inventory"
+    id = Column(Integer, primary_key=True)
+    product_id = Column(Integer, ForeignKey("Products.id"), nullable= False)
+    stock = Column(Integer, nullable= False)
+
+class BannerItems(Base):
+    __tablename__ = "BannerItems"
+    id = Column(Integer, primary_key=True)
+    product_id = Column(Integer, ForeignKey("Products.id"), nullable= False)
